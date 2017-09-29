73d748986b8d2dafc32d2f8113888129feebf3e5
Anton Iun <iunanton@naver.com>
2017-09-30 00:33:51 +0800

    Link with AccountManager

--- app/src/main/java/me/edgeconsult/authentication/AuthenticatorActivity.java
+++ app/src/main/java/me/edgeconsult/authentication/AuthenticatorActivity.java
@@ -10,22 +10,37 @@ import android.support.design.widget.TextInputLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class AuthenticatorActivity extends AccountAuthenticatorActivity {
 
     private static String TAG = "AuthenticatorActivity";
-    private TextInputLayout username;
-    private TextInputLayout password;
+    private TextInputLayout usernameInputLayout;
+    private String username;
+    private TextInputLayout passwordInputLayout;
+    private String password;
     private ProgressBar progressBar;
     private Button btn_login;
+
+    private String userName;
+    private String userPass;
 
     public static String ARG_ACCOUNT_TYPE = "ARG_ACCOUNT_TYPE";
     public static String ARG_AUTH_TYPE = "ARG_AUTH_TYPE";
     public static String ARG_IS_ADDING_NEW_ACCOUNT = "ARG_IS_ADDING_NEW_ACCOUNT";
+
     public static String ACCOUNT_TYPE = "me.edgeconsult.chat";
-    public static String PARAM_USER_PASS = "PARAM_USER_PASS";
 
     private AccountManager mAccountManager;
     private String mAuthTokenType = "";
@@ -35,8 +50,8 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authenticator);
         mAccountManager = AccountManager.get(getBaseContext());
-        username = findViewById(R.id.username_layout);
-        password = findViewById(R.id.password_layout);
+        usernameInputLayout = findViewById(R.id.username_layout);
+        passwordInputLayout = findViewById(R.id.password_layout);
         progressBar = findViewById(R.id.progressBar);
         btn_login = findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener() {
@@ -56,45 +71,69 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
         }*/
 
         btn_login.setEnabled(false);
-        username.setEnabled(false);
-        password.setEnabled(false);
+        usernameInputLayout.setEnabled(false);
+        passwordInputLayout.setEnabled(false);
         progressBar.setVisibility(View.VISIBLE);
+        username = ((EditText) findViewById(R.id.input_username)).getText().toString();
+        password = ((EditText) findViewById(R.id.input_password)).getText().toString();
 
         // Create async task here
         new AsyncTask<Void, Void, Intent>() {
             @Override
             protected Intent doInBackground(Void... voids) {
+                String urlParameters;
+                String targetURL = "http://172.17.0.6";
+                URL url;
+                HttpURLConnection connection = null;
                 try {
-                    Thread.sleep(3000);
+                    urlParameters = "username=" + URLEncoder.encode(username, "UTF-8") + "&password=" + URLEncoder.encode(password, "UTF-8");
+                    Log.i(TAG, urlParameters);
+                    url = new URL(targetURL);
+                    connection = (HttpURLConnection)url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0");
+                    connection.setRequestProperty("Accept", "*/*");
+                    connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                    connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
+                    connection.setUseCaches (false);
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+                    DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                    wr.writeBytes(urlParameters);
+                    wr.flush();
+                    wr.close();
+                    InputStream is = connection.getInputStream();
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                    String line;
+                    StringBuffer response = new StringBuffer();
+                    while((line = rd.readLine()) != null) {
+                        Log.i(TAG, line);
+                        response.append(line);
+                        response.append('\r');
+                    }
+                    rd.close();
+                    JSONObject json = new JSONObject(response.toString());
+                    if (json.has("uuid")) {
+                        String authtoken = json.getString("uuid");
+                        Log.i(TAG, authtoken);
+                        final Intent res = new Intent();
+                        res.putExtra(AccountManager.KEY_ACCOUNT_NAME, username);
+                        res.putExtra(AccountManager.KEY_ACCOUNT_TYPE, getString(R.string.account_type));
+                        res.putExtra(AccountManager.KEY_AUTHTOKEN, authtoken);
+                        return res;
+                    } else {
+                        return null;
+                    }
                 } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                } finally {
+                    if(connection != null) {
+                        connection.disconnect();
+                    }
                 }
-                return null;
             }
 
-            @Override
-            protected void onPostExecute(Intent intent) {
-                progressBar.setVisibility(View.INVISIBLE);
-                username.setEnabled(true);
-                password.setEnabled(true);
-                btn_login.setEnabled(true);
-            }
-        }.execute();
-    }
-
-    private void login2() {
-        final String userName = ((TextView) findViewById(R.id.input_username)).getText().toString();
-        final String userPass = ((TextView) findViewById(R.id.input_password)).getText().toString();
-        new AsyncTask<Void, Void, Intent>() {
-            @Override
-            protected Intent doInBackground(Void... params) {
-                String authtoken = "726e866c-9643-49e8-a26f-e26769883a5c";
-                final Intent res = new Intent();
-                res.putExtra(AccountManager.KEY_ACCOUNT_NAME, userName);
-                res.putExtra(AccountManager.KEY_ACCOUNT_TYPE, ACCOUNT_TYPE);
-                res.putExtra(AccountManager.KEY_AUTHTOKEN, authtoken);
-                res.putExtra(PARAM_USER_PASS, userPass);
-                return res;
-            }
             @Override
             protected void onPostExecute(Intent intent) {
                 finishLogin(intent);
@@ -103,18 +142,17 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
     }
 
     private void finishLogin(Intent intent) {
+        progressBar.setVisibility(View.INVISIBLE);
+        usernameInputLayout.setEnabled(true);
+        passwordInputLayout.setEnabled(true);
+        btn_login.setEnabled(true);
         String accountName = intent.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-        String accountPassword = intent.getStringExtra(PARAM_USER_PASS);
         final Account account = new Account(accountName, intent.getStringExtra(AccountManager.KEY_ACCOUNT_TYPE));
         Log.i("log",account.toString());
-        if (getIntent().getBooleanExtra(ARG_IS_ADDING_NEW_ACCOUNT, false)) {
-            String authtoken = intent.getStringExtra(AccountManager.KEY_AUTHTOKEN);
-            String authtokenType = mAuthTokenType;
-            mAccountManager.addAccountExplicitly(account, accountPassword, null);
-            mAccountManager.setAuthToken(account, authtokenType, authtoken);
-        } else {
-            mAccountManager.setPassword(account, accountPassword);
-        }
+        String authtoken = intent.getStringExtra(AccountManager.KEY_AUTHTOKEN);
+        String authtokenType = mAuthTokenType;
+        mAccountManager.addAccountExplicitly(account, null, null);
+        mAccountManager.setAuthToken(account, authtokenType, authtoken);
         setAccountAuthenticatorResult(intent.getExtras());
         setResult(RESULT_OK, intent);
         finish();
